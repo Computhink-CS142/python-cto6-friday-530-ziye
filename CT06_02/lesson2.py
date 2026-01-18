88ae31a695356be070fc51ca80608fbf00291540
Computhink-CS142 <cs142@computhink.com.sg>
2026-01-18 09:20:45 +0000

    ziye

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -4,4 +4,48 @@ print("Hello from lesson 2")
 # Using comments, translate the code shown on screen into pseudocode.
 
 ######## Write the pseudocode in comments for task 3 here
-# Using comments, translate the code shown on screen into pseudocode.
+# Using comments, translate the code shown on screen into pseudocode
+# print("red")
+# print("orange")
+# print("yellow")
+# print("green")
+print("blue")
+# print("indigo")
+# print("violet")
+
+#repeat 10 times
+#say hey
+#move 10 step
+
+# setcounter to 0
+# repest counter
+
+
+# ask "whats your age" and wait 
+#if anser less then 18 
+# say "access denied"
+#if else say "welcome"
+
+
+#create varible glass plastic and paper
+#repeat until no more items
+#pick up a item
+# if item is glass
+#put in glassbin
+#if item is plastic 
+#put in plastic bin
+#if item is paper
+#put in papaer bin
+
+
+
+#repeat forever
+#if anser =123456789
+#say congrats
+#if else
+#say error
+
+
+
+#add test1 test2 test3=total
+#saytotal
