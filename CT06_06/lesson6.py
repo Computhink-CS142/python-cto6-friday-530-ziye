3b7114b4925979842482079fe9f334a17bdc0392
Computhink-CS142 <cs142@computhink.com.sg>
2026-03-08 09:03:56 +0000

    q1

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,1 +1,75 @@
-print("Hello from lesson 6")
+# average=0
+# total=0
+
+# numstudents=input("how many students are there")
+# numstudents=int(numstudents)
+# for minecraft in range(1,numstudents+1):
+#     Q="what is the marks of the students"+str(minecraft)+"?"
+#     marks=input(Q)
+#     total=total+int(marks)
+
+# average=total/numstudents
+# print("the average for this class is",round(average,2))
+# # i am so dum!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!## Task 1: Syntax Errors
+# # Fix the errors in the following:
+
+# # **task 1a**:
+# for i in range(3):
+#     print("Hello, World!")
+
+# # Task 1b**:
+# for i in range(5):
+#     print(i)
+
+# # **Task 1c**:
+# print("Hello, World!")
+
+# # **Task 1d**:
+# roblox= 5
+
+# # **Task 1e**:
+# print ("Hello, World!")
+
+# # Task 2: Name Errors
+# # Fix the errors in the following:
+
+# # **Task 2a**:
+# print("age")
+
+# # **Task 2b**:
+# name = "Alice"
+# print(name)
+
+# # **Task 2c**:
+# x=5
+# print(x)
+
+
+# # **Task 2d**:
+# print("Hello, World!")
+## Task 3: Type Errors
+# Fix the errors in the following:
+
+# **Task 3a**:
+# age = 25
+# print(age + 1)
+
+# **Task 3b**:
+# number = 10
+# print(number - 5)
+
+# **Task 3c**:
+# print("Repeat" * 3)
+
+# **Task 3d**:
+# year = 2023
+# print("The year is " , year)
+
+# **Task 3e**:
+# x = 10
+# y = x / 2
+
+# **Task 3f**:
+# end = 5
+# for i in range(end):
+#     print(i)
--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,2 +1,101 @@
-print("Hello from lesson 7")
+# score_one = 80
+# score_two = 90
+# score_three = 75
+
+# total = score_one + score_two+score_three
+
+# average_score = total / 3
+
+# student_name = "Alex"
+
+# # print("Average score for " , student_name , " is: " , round(average_score,2))
+
+
+# for II in range(1,11):
+#     print(II)
+
+# word=input("what do yuo ehan to say idk maybe eat")
+# count=int(input("how many times to repeate?"))
+# for why_cant_it_be_dotdotdot in range(count):
+#     print (word)
+
+
+
+
+
+
+
+# blahblahblah=input("what ya wana eat")
+# blahblahblahblah=inT("how much")
+
+# for e in range(blahblahblahblah):
+#     print(blahblahblah)
+
+
+
+
+
+
+
+blahblahblah=int(input("what number dude"))
+for h in range(1,13):
+    print(blahblahblah,"x",h,"=")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
