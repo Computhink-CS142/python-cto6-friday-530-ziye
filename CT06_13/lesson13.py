5cb7c1000bbd03c49150dd50ae6f840806354cc5
Computhink-CS142 <cs142@computhink.com.sg>
2026-05-08 10:26:28 +0000

    end

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,1 +1,32 @@
-print("Hello from lesson 13")
+# number=25
+# while number<=49:
+#     print(number)
+#     number=number+1
+
+
+
+
+
+# grocery=["apples","bread","carrots","date","eggs","flour","grapes","honey",]
+# grocery[7]="herb"
+# grocery.append("ice")
+# grocery.insert(1,("banana"))
+# del(grocery[2])
+# print(len(grocery))
+# for g in grocery:
+#     if g == "apples":
+#         print("can I have 5",g)
+#     if g == "carrots":
+#         print("can I have 3",g)
+#     if g == "grapes":
+#         print("can I have the fresh farm brand ",g)
+
+
+
+
+
+
+
+riddle=input("whats a bear without a ear")
+if riddle=="B" or riddle=="b":
+    print("correct")
