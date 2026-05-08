c9eb272f13348f3753263b2133de3744a5c624b2
Computhink-CS142 <cs142@computhink.com.sg>
2026-05-08 10:26:03 +0000

    end sem test

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,1 +1,48 @@
-print("Hello from lesson 9")
+# import random
+# first_num=random.randint(1,6)
+# second_num=random.randint(1,6)
+# third_num=random.randint(1,6)
+# print("1st number",str(first_num))
+# print("2st number",str(second_num))
+# print("3st number",str(third_num))
+# first_even=first_num%2
+# second_even=second_num%2
+# third_even=third_num%2
+# all_even_or_odd=first_even==second_even==third_even
+# print("all numbers are even/odd:",str(all_even_or_odd))
+
+# user=input("how much day mr whatever")
+# if user<25:
+#     print("return ya book")
+# else:
+#     print("byebye")
+
+# user=input("how many apples you wanna buy")
+# if user>10:
+#     print("you will get 10% persent discount secret event until april!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+#     price=user-0.01
+#     print("pls pay$",price)
+# else:
+#     print(user,"this is your amt you need to pay")
+
+######################fruitifresh#######################
+num_apple=input("how much apples")
+num_orange=input("how much oranges")
+if num_apple>5:
+    print("you will have a 10% discount for all the apples")
+    apple_price=0.60*num_apple
+    apple_price=apple_price-0.1
+    print("you'll pay",apple_price)
+else:
+    apple_price=0.60*num_apple
+    print("you'll pay",apple_price)
+num_apple=input("how much apples")
+num_orange=input("how much apples")
+if num_orange>5:
+    print("you will have a 10% discount for all the apples")
+    orange_price=0.60*num_orange
+    orange_price=orange_price-0.1
+    print("you'll pay",apple_price)
+else:
+    orange_price=0.60*num_orange
+    print("you'll pay",orange_price)
--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,1 +1,75 @@
-print("Hello from lesson 11")
+# rider1=125
+# rider2=150
+# if (rider1>120 and rider2>120):
+#     print("you are both taller then 120cm")
+
+
+
+# firstname=input("what is ya first name")
+# lastname=input("whats your last name")
+# if firstname=="James" and lastname=="Leong":
+#     print("you are wanted")
+# elif  firstname=="james" and lastname=="leong":
+#     print("you are wanted")
+# else:
+#     print("you are FREE")
+
+
+
+
+# Rider1=25
+# Rider2=6
+# if (Rider1>18 or Rider2>18):
+#     print("you can ride")
+# else:
+#     print ("pls go home")
+
+
+
+# age=int(input("whats your age"))
+# if age<12 or age>65:
+#     print("please pay $12")
+# else:
+#     print("please pay $20")
+
+
+
+
+
+
+# male_or_female=input("if you are male input M if you are female input F")
+# if male_or_female=="M"or male_or_female=="male":
+#     print("valid input")
+# elif male_or_female=="F":
+#     print("invalid input")
+# else:
+#     print ("invalid inp")
+
+
+
+# day_of_the_week=input("what day is it today in british english")
+# if not day_of_the_week=="sunday" or not day_of_the_week=="saturday":
+#     print ("today is not a weekday")
+
+
+
+
+
+
+
+# password="Python123"
+# password_try=input("guess whats the password")
+# if password_try==password:
+#     print("access accepted")
+
+
+
+
+
+
+
+
+
+
+
+
